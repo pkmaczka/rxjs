0229257c43029ae68b26279fc878c972fbc9bf4d
pawel.maczka <pawel.maczka@softwareone.com>
2019-03-03 09:26:20 +0100

    Zadanie3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Zadanie1 from './zadania/Zadanie1/Zadanie1'
+import Zadanie3 from './zadania/Zadanie3/Zadanie3'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Zadanie1 />
+          <Zadanie3 />
         </header>
       </div>
     );
---
+++ src/zadania/Zadanie3/Zadanie3.jsx
@@ -0,0 +1,88 @@
+// Zabawa
+
+import React, { Component } from 'react'
+import ReactJson from 'react-json-view'
+import { Subject, of, empty } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { tap, debounceTime, distinctUntilChanged } from 'rxjs/operators'
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+
+
+const cities = [
+    'Warsaw',
+    'Cape Town',
+    'Singapore',
+    'New York',
+    'San Francisco',
+    'Tokyo'
+]
+
+// 1. Dodać funkcjonalność licznika przy pomocy funkcji scan
+// 2. Dodać debounce na kliknięcia
+
+export default class Zadanie3 extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            weather: {
+                city: {
+                    name: 'Unknown'
+                },
+                list: [
+                    {
+                        main: {
+                            temp: -274
+                        }
+                    }
+                ]
+            }
+        }
+        this.inputRef = React.createRef();
+        this.subject = new Subject();
+    }
+
+    componentDidMount() {
+        this.subject
+        .pipe(
+           debounceTime(1000),
+           distinctUntilChanged(),
+           map(x => x*2),
+           map(x => x%cities.length),
+           map(x => cities[x]),
+           tap(x => console.log(`Fetching weather from ${x}`)),
+           switchMap(city =>
+            ajax.getJSON(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=4550d02a49c044c506beae2dc1debbaf&units=metric`)
+            .pipe(
+                catchError(() => empty())
+            ))
+        )
+        .subscribe((x) => {
+            this.setState({
+                weather: x
+            });
+            console.log(x);
+        });
+    }
+
+    sendEvent = (event) => {
+        this.subject.next(event.target.value)
+    }
+
+    render = () => (
+        <>
+            <Input type="text" onChange={this.sendEvent} />
+            <br /><br />
+            { this.state.weather.city.name !== 'Unknown' && <Paper>
+                <Typography variant="h2">
+                    {this.state.weather.city.name} ({this.state.weather.list[0].main.temp})
+                    </Typography>
+                <ReactJson src={this.state.weather} />
+            </Paper>}
+        </>
+    )
+}
